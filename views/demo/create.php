9c3f316fb6c9cd279ace066561fcc161036249f2
Pappoy2910 <indah.hasdar29@gmail.com>
2023-05-30 19:32:06 +0800

    first commit

---
+++ views/demo/create.php
@@ -0,0 +1,20 @@
+<?php
+
+use yii\helpers\Html;
+
+/** @var yii\web\View $this */
+/** @var app\models\demo $model */
+
+$this->title = 'Create Demo';
+$this->params['breadcrumbs'][] = ['label' => 'Demos', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="demo-create">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?= $this->render('_form', [
+        'model' => $model,
+    ]) ?>
+
+</div>
